b3629f8cb3015f7901de69f0c15318ea39ca221a
Anabel VM <anabelvivasm@gmail.com>
2024-12-04 10:59:19 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronombres = ["el", "la", "nuestro", "mi", "tu"];
+  let adjetivos = ["divertido", "genial", "mágico", "fantástico", "colorido"];
+  let sustantivos = [
+    "juguete",
+    "juguetería",
+    "muñeco",
+    "juguetón",
+    "juegolandia"
+  ];
+
+  for (let i = 0; i < pronombres.length; i++) {
+    for (let j = 0; j < adjetivos.length; j++) {
+      for (let k = 0; k < sustantivos.length; k++) {
+        let nombreDominio =
+          pronombres[i] + adjetivos[j] + sustantivos[k] + ".com";
+
+        console.log(nombreDominio);
+      }
+    }
+  }
 };
